72d861c9c7cebbf6dca45d2f8935a0aa2628db56
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-08-02 20:14:05 +0200

    all

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,12 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    // Initialize all modals
+    console.log("Initializing modals...");
+    let  md = document.querySelectorAll('.modal');
+    console.log("modals...", md);
+     M.Modal.init(md);
+
     // Initialize all sidenav elements
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // Datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    options = {
+      format: "dd mmmm yyyy",
+      i18n: {
+        done: "Select"
+    }
+    };
+    M.Datepicker.init(datepicker, options);
+
+    // Initialize all select elements
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
+    // Initialize all collapsible elements
+    let collapsible = document.querySelectorAll('.collapsible');
+    console.log("collapsible...", collapsible);
+    M.Collapsible.init(collapsible);
+
   });
-
-  // Or with jQuery
-
-//   $(document).ready(function(){
-//     $('.sidenav').sidenav();
-//   });
-        
