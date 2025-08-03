f52c61b04ef6c82641100ad0ba77d9d20cbec43a
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-08-03 09:50:47 +0200

    all3

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -9,14 +9,14 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+print("DATABASE_URL------", os.environ.get("DEVELOPMENT"))
 
-if os.environ.get("DEVELOPMENT") == True:
+if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-        
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 
